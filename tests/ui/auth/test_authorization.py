bd29f2a3baea42ef61685256ffd87aab36d5255d
lexaeliseev <lexaeliseev@gmail.com>
2024-12-19 20:32:57 +0300

    add test_auth

---
+++ tests/ui/auth/test_authorization.py
@@ -0,0 +1,34 @@
+import os
+
+import allure
+import pytest
+from dotenv import load_dotenv
+
+from page.auth_page import auth_page
+
+load_dotenv()
+
+
+@allure.label("owner", "aa.eliseev")
+@allure.feature('Поиск')
+@allure.tag("Smoke")
+@allure.title('Успешная авторизация в системе')
+def test_auth_success(setup_browser):
+    auth_page.open_page()
+    auth_page.click_menu_login()
+    auth_page.auth_success(os.getenv('STEPIK_EMAIL'), os.getenv('STEPIK_PASSWORD'))
+
+
+@pytest.mark.parametrize(
+    'login, password',
+    [(os.getenv('STEPIK_EMAIL'), 'password'),
+     ('login@test.com', os.getenv('STEPIK_PASSWORD'))],
+    ids=['Login1', 'Login2'])
+@allure.label("owner", "aa.eliseev")
+@allure.feature('Поиск')
+@allure.tag("Smoke")
+@allure.title('Авторизация с невалидными данными')
+def test_auth_failure(setup_browser, login, password):
+    auth_page.open_page()
+    auth_page.click_menu_login()
+    auth_page.auth_failure(login, password)
