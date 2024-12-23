b2eed9e32799c8efce3605bb53d71ef33529751c
lexaeliseev <lexaeliseev@gmail.com>
2024-12-23 18:44:51 +0300

    fix test

--- tests/ui/auth/test_authorization.py
+++ tests/ui/auth/test_authorization.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from page.auth_page import auth_page
 
 
-
 @allure.label("owner", "aa.eliseev")
 @allure.feature('Поиск')
 @allure.tag("Smoke")
@@ -18,17 +17,11 @@ def test_auth_success(setup_browser):
     auth_page.auth_success(os.getenv('STEPIK_EMAIL'), os.getenv('STEPIK_PASSWORD'))
 
 
-@pytest.mark.parametrize(
-    'login, password',
-    [(os.getenv('STEPIK_EMAIL'), 'password'),
-     ('login@test.com', os.getenv('STEPIK_PASSWORD'))],
-    ids=['Login1', 'Login2'])
 @allure.label("owner", "aa.eliseev")
 @allure.feature('Поиск')
 @allure.tag("Smoke")
 @allure.title('Авторизация с невалидными данными')
-def test_auth_failure(setup_browser, login, password):
-    load_dotenv()
+def test_auth_failure(setup_browser):
     auth_page.open_page()
     auth_page.click_menu_login()
-    auth_page.auth_failure(login, password)
+    auth_page.auth_failure("login", "password")
