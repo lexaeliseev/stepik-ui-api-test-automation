6f563772d99857f616292b63ffc8d62a3ee6c72b
lexaeliseev <lexaeliseev@gmail.com>
2024-12-24 17:36:27 +0300

    change in tests

--- tests/ui/auth/test_authorization.py
+++ tests/ui/auth/test_authorization.py
@@ -1,27 +1,53 @@
 import os
-
 import allure
-import pytest
-from dotenv import load_dotenv
-
 from page.auth_page import auth_page
 
 
 @allure.label("owner", "aa.eliseev")
+@allure.epic('UI')
 @allure.feature('Авторизация')
 @allure.tag("Smoke")
 @allure.title('Успешная авторизация в системе')
 def test_auth_success(setup_browser):
     auth_page.open_page()
     auth_page.click_menu_login()
-    auth_page.auth_success(os.getenv('STEPIK_EMAIL'), os.getenv('STEPIK_PASSWORD'))
+    auth_page.fill_login(os.getenv('STEPIK_EMAIL'))
+    auth_page.fill_password(os.getenv('STEPIK_PASSWORD'))
+    auth_page.auth_click()
+    auth_page.click_user_menu('Профиль')
+    auth_page.check_success_auth('Тестовый аккаунт')
 
 
 @allure.label("owner", "aa.eliseev")
+@allure.epic('UI')
+@allure.feature('Авторизация')
+@allure.tag("Smoke")
+@allure.title('Выход авторизованного пользователя из системы')
+def test_auth_logout(setup_browser):
+
+    # заменить этот блок апишной авторизацией
+    auth_page.open_page()
+    auth_page.click_menu_login()
+    auth_page.fill_login(os.getenv('STEPIK_EMAIL'))
+    auth_page.fill_password(os.getenv('STEPIK_PASSWORD'))
+    auth_page.auth_click()
+
+    auth_page.click_user_menu('Профиль')
+    auth_page.check_success_auth('Тестовый аккаунт')
+
+    auth_page.click_user_menu('Выход')
+    auth_page.check_success_logout()
+
+
+@allure.label("owner", "aa.eliseev")
+@allure.epic('UI')
 @allure.feature('Авторизация')
 @allure.tag("Smoke")
 @allure.title('Авторизация с невалидными данными')
 def test_auth_failure(setup_browser):
     auth_page.open_page()
     auth_page.click_menu_login()
-    auth_page.auth_failure("login@test.com", "password")
+    auth_page.fill_login('login@test.com')
+    auth_page.fill_password('password')
+    auth_page.auth_click()
+    auth_page.check_auth_failure()
