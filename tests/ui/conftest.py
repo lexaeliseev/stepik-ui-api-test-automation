582503563aa553c59e684aab55320e374dce96a6
lexaeliseev <lexaeliseev@gmail.com>
2025-01-12 16:22:27 +0300

    change conftest for ui tests

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -6,7 +6,7 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    browser.config.timeout = 10
+    browser.config.timeout = 12
     browser.config.window_height = 1080
     browser.config.window_width = 1920
     browser.config.base_url = os.getenv('URL')
