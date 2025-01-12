06ceb33e2505017ae2a39575dff2294a03a092e9
lexaeliseev <lexaeliseev@gmail.com>
2025-01-12 17:09:08 +0300

    change utils

--- utils/utils.py
+++ utils/utils.py
@@ -5,14 +5,6 @@ from requests import Response
 from allure_commons.types import AttachmentType
 
 
-# def response_logging(response: Response):
-#     logging.info("Request: " + response.request.url)
-#     if response.request.body:
-#         logging.info("INFO Request body: " + response.request.body)  # логирование тела запроса если оно есть
-#     logging.info("Request headers: " + str(response.request.headers))
-#     logging.info("Response code " + str(response.status_code))
-#     logging.info("Response: " + response.text)
-
 def response_logging(response: Response):
     logging.info("Request: " + response.request.url)
     if response.request.body:
